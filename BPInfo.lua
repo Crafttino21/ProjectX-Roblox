ebf4798fd76d0733a5c46283a4432d885f5a64e1
WeepingAngel <58913870+Crafttino21@users.noreply.github.com>
2025-03-12 19:53:53 +0100

    Update BPInfo.lua

--- BPInfo.lua
+++ BPInfo.lua
@@ -1,21 +1,13 @@
-﻿print    _           _
-print _/ \ _______ / \_
-print /  __/__   __\__  \
-print \_/____ \ / ____\_/
-print  //    \___/    \\
-print | |   o _ _ o   | |
-print  \_\___/ V \___/_/
-print  < `    | |    ' >
-print   \__.  \^/  .__/
-print     >         <
-print     [_I_I_I_I_]
-print     /  /   \  \
-print   _/  /     \  \_
-print  /   <       >   \
-print  \_.  |     |  ._/
-print    \_/       \_/
-print "Roblox Byfron Anticheat Bypass by: "
-print "WeepingAngel, game-hax, WeAreDevs Team"
-print "Fuck the Shit out of this game! WE ARE IN!!"
-print "Project X to the MOON! Follow me on GitHub: https://github.com/Crafttino21"
-print "You are untasteble!"
+print "██████╗ ██████╗  ██████╗      ██╗███████╗ ██████╗████████╗    ██╗  ██╗    ██████╗ "
+print "██╔══██╗██╔══██╗██╔═══██╗     ██║██╔════╝██╔════╝╚══██╔══╝    ╚██╗██╔╝    ╚════██╗"
+print "██████╔╝██████╔╝██║   ██║     ██║█████╗  ██║        ██║        ╚███╔╝      █████╔╝"
+print "██╔═══╝ ██╔══██╗██║   ██║██   ██║██╔══╝  ██║        ██║        ██╔██╗     ██╔═══╝ "
+print "██║     ██║  ██║╚██████╔╝╚█████╔╝███████╗╚██████╗   ██║       ██╔╝ ██╗    ███████╗"
+print "╚═╝     ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚══════╝ ╚═════╝   ╚═╝       ╚═╝  ╚═╝    ╚══════╝"                                                                             
+print "Project X 2 successfully set up!"
+print "Developer: WeepingAngel"
+print "Check out our Discord: .gg/q3KRw62gTx"
+print "System checks:"
+print "Injection Method: successful"
+print "BTInjector: successful"
+print "Loadstring: successful"
